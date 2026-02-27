6c3d433fca7073a8a418dfccb943364fd25bc23f
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-02-28 08:47:47 +0900

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -251,6 +251,11 @@ class CarInterfaceBase(ABC):
       elif not cs_out.cruiseState.enabled:
         events.add(EventName.pcmDisable)
 
+    # 오토 인게이지
+    if cs_out.cruiseState.enabled:
+      if cs_out.gearShifter == GearShifter.drive and cs_out.vEgo > 5. * CV.KPH_TO_MS:
+        events.add(EventName.pcmEnable)
+        
     return events
 
 
