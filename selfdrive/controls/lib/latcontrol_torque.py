b1e634797f31fb6f0a963037e6e28385eb70d532
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-01 00:41:20 +0900

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -44,6 +44,9 @@ class LatControlTorque(LatControl):
       output_torque = 0.0
       pid_log.active = False
       angle_steers_des = 0.0
+      pid_log.latAccelFactor = self.torque_params.latAccelFactor
+      pid_log.latAccelOffset = self.torque_params.latAccelOffset
+      pid_log.friction = self.torque_params.friction
     else:
       if self.use_steering_angle:
         actual_curvature = -VM.calc_curvature(math.radians(CS.steeringAngleDeg - params.angleOffsetDeg), CS.vEgo, params.roll)
