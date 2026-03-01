970373a834e98ec1f4ebd0d6251eb83fbbf8df2e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-01 18:44:27 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -43,10 +43,10 @@ ACADOS_SOLVER_TYPE = 'SQP_RTI'
 
 
 CRUISE_GAP_BP = [1., 2., 3., 4.]
-CRUISE_GAP_V = [1.0, 1.2, 1.4, 2.0]
+CRUISE_GAP_V = [1.0, 1.5, 2.0, 2.0]
 
 AUTO_TR_BP = [0., 30.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 110.*CV.KPH_TO_MS]
-AUTO_TR_V = [1.0, 1.2, 1.3, 1.45]
+AUTO_TR_V = [1.0, 1.1, 1.25, 1.45]
 
 AUTO_TR_CRUISE_GAP = 4
 DIFF_RADAR_VISION = 1.0
@@ -62,8 +62,8 @@ T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 T_FOLLOW = 1.45
-COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 6.5
+COMFORT_BRAKE = 2.3
+STOP_DISTANCE = 7.0
 
 def get_stopped_equivalence_factor(v_lead):
   return (v_lead**2) / (2 * COMFORT_BRAKE)
