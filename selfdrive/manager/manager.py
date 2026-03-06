39f20cf6dca62d88486cf24eb342f6b6455cba21
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-06 13:23:14 +0900

    Update manager.py

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -68,9 +68,6 @@ def manager_init() -> None:
   if params.get_bool("RecordFrontLock"):
     params.put_bool("RecordFront", True)
 
-  if not params.get_bool("DisableRadar_Allow"):
-    params.delete("DisableRadar")
-
   # set unset params
   for k, v in default_params:
     if params.get(k) is None:
