5d32c9b3599a5c89b0d62a8f936d915d7384be85
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-06 12:55:53 +0900

    Update ui.h

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -108,7 +108,7 @@ typedef struct UIScene {
   bool lead_radar[2] = {false, false};
 
   float light_sensor, accel_sensor, gyro_sensor;
-  bool started, ignition, is_metric, longitudinal_control, end_to_end;
+  bool started, ignition, is_metric, longitudinal_control, end_to_end, end_to_end_long;
   uint64_t started_frame;
 } UIScene;
 
