c69042ba75f24811dffc0f97441cfb29731a8bd3
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-06 13:41:40 +0900

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -96,6 +96,7 @@ class LongControl:
       v_target_1sec = interp(self.CP.longitudinalActuatorDelayUpperBound + t_since_plan + 1.0, T_IDXS[:CONTROL_N], speeds)
     else:
       v_target = 0.0
+      v_target_now = 0.0
       v_target_1sec = 0.0
       a_target = 0.0
 
