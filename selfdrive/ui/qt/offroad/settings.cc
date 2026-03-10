ae31a16258fe06bac666168035bd51ffb0ea02fb
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-10 12:34:00 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -66,9 +66,9 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
       "../assets/offroad/icon_monitoring.png",
     },
     {
-      "EndToEndToggle",
-      "\U0001f96c Disable use of lanelines (Alpha) \U0001f96c",
-      "In this mode openpilot will ignore lanelines and just drive how it thinks a human would.",
+      "UseLanelines",
+      tr("Use lane lines instead of e2e"),
+      "",
       "../assets/offroad/icon_road.png",
     },
     {
