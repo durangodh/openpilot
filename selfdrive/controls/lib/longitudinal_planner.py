ed1a14471adb8f608eca761eea270646f96e8500
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-10 11:22:01 +0900

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -142,7 +142,7 @@ class LongitudinalPlanner:
     self.j_desired_trajectory = np.interp(T_IDXS[:CONTROL_N], T_IDXS_MPC[:-1], self.mpc.j_solution)
 
     # TODO counter is only needed because radar is glitchy, remove once radar is gone
-    self.fcw = self.mpc.mode == 'acc' and self.mpc.crash_cnt > 5
+    self.fcw = self.mpc.mode == 'acc' and self.mpc.crash_cnt > 5 and not sm['carState'].standstill
     if self.fcw:
       cloudlog.info("FCW triggered")
 
