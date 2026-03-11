1456636e6a451a9726c00e0e35a8188e596ca621
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-11 17:34:01 +0900

    Update onroad.h

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -38,6 +38,7 @@ class NvgWindow : public CameraViewWidget {
 
 public:
   explicit NvgWindow(VisionStreamType type, QWidget* parent = 0);
+  void updateState(const UIState &s);
 
 signals:
   void valueChanged();
@@ -51,6 +52,7 @@ protected:
   void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd, bool is_radar);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
   inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
+  inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
 
   double prev_draw_t = 0;
   bool engageable = false;
