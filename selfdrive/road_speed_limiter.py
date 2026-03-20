6f8dd4898847a91a279b81d4c457362723845201
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-03-20 15:30:24 +0900

    Update road_speed_limiter.py

--- selfdrive/road_speed_limiter.py
+++ selfdrive/road_speed_limiter.py
@@ -343,7 +343,7 @@ class RoadSpeedLimiter:
         diff_speed = cluster_speed - (cam_limit_speed * camSpeedFactor)
         #cam_limit_speed_ms = cam_limit_speed * (CV.KPH_TO_MS if is_metric else CV.MPH_TO_MS)
 
-        starting_dist = v_ego * 30.
+        starting_dist = v_ego * 25.
 
         if cam_type == 22:
           safe_dist = v_ego * 3.
