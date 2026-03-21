361ecdf0c6fc48b879a357dff1f83ebc97780519
durango-op <71580184+durango-op@users.noreply.github.com>
2026-03-21 09:13:21 +0900

    Revert "Update controlsd.py"
    This reverts commit 416dcf2ce558f845f8ef4fdd8696df4d42a6c6d3.

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -614,8 +614,7 @@ class Controls:
       self.desired_curvature, self.desired_curvature_rate = get_lag_adjusted_curvature(self.CP, CS.vEgo,
                                                                                        lat_plan.psis,
                                                                                        lat_plan.curvatures,
-                                                                                       lat_plan.curvatureRates,
-                                                                                       lat_plan.distances)
+                                                                                       lat_plan.curvatureRates)
       actuators.steer, actuators.steeringAngleDeg, lac_log = self.LaC.update(CC.latActive, CS, self.VM, params, 
                                                                              self.last_actuators, self.steer_limited, self.desired_curvature,
                                                                              self.desired_curvature_rate, self.sm['liveLocationKalman'])
