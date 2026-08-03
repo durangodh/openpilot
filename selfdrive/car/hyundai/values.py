d15ab2fc25e2aee3e207d61b632e39a6e08874fb
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-08-03 22:34:08 +0900

    현대 SCC 가감속 한계 캐럿값 적용

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -9,8 +9,8 @@ Ecu = car.CarParams.Ecu
 
 class CarControllerParams:
 
-  ACCEL_MAX = 2.0
-  ACCEL_MIN = -3.5
+  ACCEL_MAX = 2.5
+  ACCEL_MIN = -4.0
 
   def __init__(self, CP):
     self.STEER_MAX = 409   # 409 is the max, 255 is stock
