aa73deefd88fbe59365777249c1f8c099537c8ac
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-08-04 16:41:18 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -163,6 +163,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"TermsVersion", PERSISTENT},
     {"Timezone", PERSISTENT},
     {"TrainingVersion", PERSISTENT},
+    {"TFollowSpeedRatio", PERSISTENT},
     {"UpdateAvailable", CLEAR_ON_MANAGER_START},
     {"UpdateFailedCount", CLEAR_ON_MANAGER_START},
     {"Version", PERSISTENT},
