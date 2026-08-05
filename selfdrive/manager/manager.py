c3e832dd987aa40c7dbd9c3042960085f5ae120e
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-08-05 23:19:01 +0900

    Remove cruise acceleration override default

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -58,7 +58,6 @@ def manager_init() -> None:
     ("UseClusterSpeed", "0"),
     ("LongControlEnabled", "0"),
     ("CruiseSpeedMin", "30"),
-    ("CruiseSpeedUpAccelLimit", "80"),
     ("JerkStartLimit", "10"),
     ("IsLdwsCar", "0"),
     ("LaneChangeEnabled", "0"),
