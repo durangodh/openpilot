ed5f51fa8064aaca5505c0979f8798dc73f24a19
durango-op <71580184+durango-op@users.noreply.github.com>
2026-08-05 13:05:27 +0900

    Enlarge all parameter touch controls

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -61,14 +61,15 @@ OffsetTotalControl::OffsetTotalControl(const QString &title,
 
   const QString btn_style = R"(
     QPushButton {
-      font-size: 40px;
+      font-size: 48px;
       font-weight: bold;
-      border-radius: 10px;
+      border-radius: 14px;
       background-color: #393939;
       color: #ffffff;
-      min-width: 80px;
-      max-width: 80px;
-      min-height: 70px;
+      min-width: 150px;
+      max-width: 150px;
+      min-height: 100px;
+      max-height: 100px;
     }
     QPushButton:pressed { background-color: #4a4a4a; }
   )";
@@ -127,14 +128,15 @@ AdjustLaneOffsetControl::AdjustLaneOffsetControl(const QString &title,
 
   const QString btn_style = R"(
     QPushButton {
-      font-size: 40px;
+      font-size: 48px;
       font-weight: bold;
-      border-radius: 10px;
+      border-radius: 14px;
       background-color: #393939;
       color: #ffffff;
-      min-width: 80px;
-      max-width: 80px;
-      min-height: 70px;
+      min-width: 150px;
+      max-width: 150px;
+      min-height: 100px;
+      max-height: 100px;
     }
     QPushButton:pressed { background-color: #4a4a4a; }
   )";
