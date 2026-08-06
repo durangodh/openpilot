0689d5a8a5994f76332d599d862a27af2a9dde72
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-08-07 08:10:57 +0900

    Move lane debug display left

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -804,7 +804,7 @@ void NvgWindow::drawCarrotBottom(QPainter &p) {
     }
   }
   if (!lat_debug.isEmpty()) {
-    int right_limit = width() - 20;
+    int right_limit = width() - 180;
     int left_limit = 240;
     int avail = right_limit - left_limit;
 
