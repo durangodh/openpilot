8f6a1c4ef65756e0d1c589cbd5fbd8e7e8abc544
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-08-06 10:37:20 +0900

    Remove obsolete ATC animation default

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -74,7 +74,6 @@ def manager_init() -> None:
     ("NewRadarInterface", "0"),
     ("WideCameraOnly", "0"),       # plannerd.py 크래시 수정
     ("ShowGearAnimation", "1"),
-    ("ShowAtcAnimation", "1"),
     ("ShowCarrotHud", "1"),
     ("CustomSteerRatio", "1650"),
     ("UseLiveSteerRatio", "0"),
