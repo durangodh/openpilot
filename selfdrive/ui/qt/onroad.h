cb9a7a89e44ada77a1dc0a6d6cbd4c59069b44e1
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-08-07 23:25:47 +0900

    Update onroad.h

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -30,8 +30,6 @@ class NvgWindow : public CameraViewWidget {
   Q_PROPERTY(bool experimentalMode MEMBER experimentalMode NOTIFY valueChanged);
   Q_PROPERTY(int status MEMBER status NOTIFY valueChanged);
   Q_PROPERTY(float ang_str MEMBER ang_str NOTIFY valueChanged);
-  Q_PROPERTY(bool left_blindspot MEMBER left_blindspot);   // blind spot
-  Q_PROPERTY(bool right_blindspot MEMBER right_blindspot); // blind spot
 
 public:
   explicit NvgWindow(VisionStreamType type, QWidget* parent = 0);
@@ -46,7 +44,6 @@ protected:
   void showEvent(QShowEvent *event) override;
   void updateFrameMat(int w, int h) override;
   void drawLaneLines(QPainter &painter, const UIState *s);
-  void drawBlindSpot(QPainter &painter, const line_vertices_data &vd, const QColor &color);
   void drawCarrotLead(QPainter &p);
 
   inline QColor redColor(int alpha = 255)   { return QColor(201, 34, 49, alpha); }
@@ -58,9 +55,6 @@ protected:
   bool experimentalMode = false;
   int status = STATUS_DISENGAGED;
   float ang_str = 0;
-
-  bool left_blindspot  = false; // blind spot
-  bool right_blindspot = false; // blind spot
   
   FirstOrderFilter fps_filter;
   FirstOrderFilter accel_filter;
@@ -113,7 +107,6 @@ protected:
   int  carrot_atc_end_time = 6;
   int  show_datetime = 1;
   int  show_gear_animation = 1;
-  int  show_bsd_always = 0;
   int  show_carrot_hud = 1;
   int  show_path_status_color = 1;
   int  show_path_brake_border = 1;
@@ -174,6 +167,10 @@ private:
   OnroadAlerts *alerts;
   NvgWindow *nvg;
   QColor bg = bg_colors[STATUS_DISENGAGED];
+  bool brake_lights = false;
+  bool left_blindspot = false;
+  bool right_blindspot = false;
+  float steering_angle_deg = 0.0f;
   QWidget *map = nullptr;
   QHBoxLayout *split;
 
