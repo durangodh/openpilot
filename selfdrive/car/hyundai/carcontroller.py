43da3b18ca9778d887ce6f56995c3fa757a326ff
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-08-08 17:18:17 +0900

    Restore MAD mode cancel handling

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -64,6 +64,7 @@ class CarController:
 
     param = Params()
 
+    self.mad_mode_enabled = param.get_bool('MadModeEnabled')
     self.ldws_opt = param.get_bool('IsLdwsCar')
     self.stock_navi_decel_enabled = param.get_bool('StockNaviDecelEnabled')
     self.keep_steering_turn_signals = param.get_bool('KeepSteeringTurnSignals')
@@ -167,8 +168,8 @@ class CarController:
     if self.frame % 2 and CS.mdps_bus:  # send clu11 to mdps if it is not on bus 0
       can_sends.append(create_clu11(self.packer, CS.mdps_bus, CS.clu11, Buttons.NONE, enabled_speed))
 
-    #if pcm_cancel_cmd and (self.longcontrol and not self.mad_mode_enabled):
-    #  can_sends.append(create_clu11(self.packer, CS.scc_bus, CS.clu11, Buttons.CANCEL, clu11_speed))
+    if pcm_cancel_cmd and (self.longcontrol and not self.mad_mode_enabled):
+      can_sends.append(create_clu11(self.packer, CS.scc_bus, CS.clu11, Buttons.CANCEL, clu11_speed))
 
     if CS.mdps_bus or self.car_fingerprint in FEATURES["send_mdps12"]:  # send mdps12 to LKAS to prevent LKAS error
       can_sends.append(create_mdps12(self.packer, self.frame, CS.mdps12))
