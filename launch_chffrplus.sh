8ae083762d222a9455b71f81bf379786d1536d0e
durangodh <71438711+durangodh@users.noreply.github.com>
2026-09-06 12:39:33 +0900

    fix(launch): run manager through python3

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -186,7 +186,7 @@ function launch {
 
   # start manager
   cd selfdrive/manager
-  ./build.py && ./manager.py
+  ./build.py && python3 ./manager.py
 
   # if broken, keep on screen error
   while true; do sleep 1; done
